5bc8f7b8a3bae016c03ce931385b9a850d2082aa
KimCCode <120701687+KimCCode@users.noreply.github.com>
2024-01-03 19:28:07 +1100

    Finished authentication for all /log routes

--- server/src/controllers/log.ts
+++ server/src/controllers/log.ts
@@ -4,6 +4,7 @@ import jwt from 'jsonwebtoken';
 
 const createLog = (req: Request, res: Response) => {
   const token = req.header('token');
+
   jwt.verify(token, process.env.JWT_SECRET, async (err: jwt.VerifyErrors, decoded: jwt.JwtPayload) => {
     // Primarily checks if token time has expired
     if (err) {
@@ -71,56 +72,80 @@ const getLog = (req: Request, res: Response) => {
 };
 
 const deleteLog = async (req: Request, res: Response) => {
-  // const userID = req.body.userID;
+  const token = req.header('token');
   const logID = String(req.params.logID);
-  try {
-    // Check if user exists
-    // const user = await UserDB.findById(userID);
-    // if (!user) {
-    //   return res.status(404).json({ msg: `No user with id: ${userID}` });
-    // }
 
-    const log = await LogDB.findById(logID);
-    if (!log) {
-      return res.status(404).json({ msg: `No log with id: ${logID}` });
+  jwt.verify(token, process.env.JWT_SECRET, async (err: jwt.VerifyErrors, decoded: jwt.JwtPayload) => {
+    if (err) {
+      if (err.name === 'TokenExpiredError') {
+        return res.status(401).json({ msg: 'Token has expired' });
+      } else {
+        return res.status(401).json({ msg: 'Token verification failed' });
+      }
+    } else {
+      const userID = decoded.userID;
+      try {
+        // Check if user exists
+        const user = await UserDB.findById(userID);
+        if (!user) {
+          return res.status(404).json({ msg: `No user with id: ${userID}` });
+        }
+
+        const log = await LogDB.findById(logID);
+        if (!log) {
+          return res.status(404).json({ msg: `No log with id: ${logID}` });
+        }
+
+        if (String(log.ownerID) !== userID) {
+          return res.status(403).json({ msg: 'User does not have access' });
+        }
+        EntryDB.deleteMany({ logID })
+          .then(() => {
+            console.log('Entries successfully deleted');
+          })
+          .catch(() => {
+            return res.status(404).json({ msg: 'No entry with id' });
+          });
+
+        LogDB.findByIdAndDelete(logID)
+          .then(() => {
+          // Should delete entries in log as well
+            res.json({ status: 'success' });
+          })
+          .catch(() => {
+            return res.status(404).json({ msg: `No log with id: ${logID}` });
+          });
+      } catch (error) {
+        return res.status(500).json({ msg: error });
+      }
     }
-
-    // if (log.ownerID !== userID) {
-    //   return res.status(403).json({ msg: 'User does not have access' });
-    // }
-    EntryDB.deleteMany({ logID })
-      .then(() => {
-        console.log('Entries successfully deleted');
-      })
-      .catch(() => {
-        return res.status(404).json({ msg: 'No entry with id' });
-      });
-
-    LogDB.findByIdAndDelete(logID)
-      .then(() => {
-      // Should delete entries in log as well
-        res.json({ status: 'success' });
-      })
-      .catch(() => {
-        return res.status(404).json({ msg: `No log with id: ${logID}` });
-      });
-  } catch (error) {
-    return res.status(500).json({ msg: error });
-  }
+  });
 };
 
 const viewLogsOwned = async (req: Request, res: Response) => {
-  // const userID = req.body.userID;
-  try {
-    // const user = await UserDB.findById(userID);
-    // if (!user) {
-    //   return res.status(404).json({ msg: `No user with id: ${userID}` });
-    // }
-    const logsOwned = await LogDB.find({});
-    res.json({ logsOwned } );
-  } catch (error) {
-    return res.status(500).json({ msg: error });
-  }
+  const token = req.header('token');
+
+  jwt.verify(token, process.env.JWT_SECRET, async (err: jwt.VerifyErrors, decoded: jwt.JwtPayload) => {
+    if (err) {
+      if (err.name === 'TokenExpiredError') {
+        return res.status(401).json({ msg: 'Token has expired' });
+      } else {
+        return res.status(401).json({ msg: 'Token verification failed' });
+      }
+    } else {
+      const userID = decoded.userID;
+      try {
+        const user = await UserDB.findById(userID);
+        if (!user) {
+          return res.status(404).json({ msg: `No user with id: ${userID}` });
+        }
+        const logsOwned = await LogDB.find({ ownerID: userID });
+        res.json({ logsOwned } );
+      } catch (error) {
+        return res.status(500).json({ msg: error });
+      }
+    }
+  });
 };
 
 export {
